a91a4f8d922c8ec3ef47ddaad80091db2055d5ca
jominjimail <jominjimail@gmail.com>
2020-01-05 05:47:44 +0900

    [Feat] 유저의 프로필 사진과 대표 썸네일을 변경할 수 있음
    기본 이미지를 자신의 자신으로 변경가능

--- api/register/index.js
+++ api/register/index.js
@@ -1,10 +1,18 @@
 const expres = require('express');
 
 const router = expres.Router();
-
+const User = require('../../model/user');
 const controller = require('./register.controller');
 const multerUpload = require('../../middleware/multer');
 
 router.post('/:userId', multerUpload, controller.registerPortfolio);
 
+router.post('/me/:userId', multerUpload, controller.registerMe);
+
+router.get('/test', async () => User.create({
+    email: 'jominjimail@hanyang.ac.kr',
+    password: '1234',
+    nickname: 'minij',
+    description: '설명',
+}));
 module.exports = router;
