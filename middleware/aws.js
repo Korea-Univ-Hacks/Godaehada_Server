4f3d685ee114ab1704fff4e29fbf08c7450f8380
sangyeol <twysg@bbros.kr>
2020-01-05 09:13:12 +0900

    Fix: 포트폴리오 생성 API 버그 수정

--- middleware/aws.js
+++ middleware/aws.js
@@ -3,7 +3,6 @@ const AWS = require('aws-sdk');
 
 const accessKey = process.env.OS_ACCESS_KEY;
 const secretKey = process.env.OS_SECRET_KEY;
-console.log(accessKey, secretKey);
 const config = {
     accessKeyId: accessKey,
     secretAccessKey: secretKey,
